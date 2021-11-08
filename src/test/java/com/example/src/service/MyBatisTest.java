cadca1eea456fed367a0ea7d35ad29fc180f82a9
hjun-park <phj0860@gmail.com>
2021-11-08 23:30:17 +0900

    테스트 코드 추가

--- src/test/java/com/example/src/service/MyBatisTest.java
+++ src/test/java/com/example/src/service/MyBatisTest.java
@@ -1,33 +1,31 @@
-//package com.example.src.service;
-//
-//import org.apache.ibatis.session.SqlSession;
-//import org.apache.ibatis.session.SqlSessionFactory;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//
-//import javax.inject.Inject;
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations = {"file:/web/WEB-INF/spring/applicationContext.xml"})
-//public class MyBatisTest {
-//
-//	@Inject
-//	private SqlSessionFactory sqlSessionFactory;
-//
-//	@Test
-//	public void testFactory() {
-//		System.out.println(sqlSessionFactory);
-//	}
-//
-//	@Test
-//	public void testSession() throws Exception {
-//		try (SqlSession session = sqlSessionFactory.openSession()) {
-//			System.out.println(session);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//}
+package com.example.src.service;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:web/WEB-INF/applicationContext.xml"})
+public class MyBatisTest {
+
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
+
+	@Test
+	public void testFactory() {
+		System.out.println(sqlSessionFactory);
+	}
+
+	@Test
+	public void testSession() throws Exception {
+		try (SqlSession session = sqlSessionFactory.openSession()) {
+			System.out.println(session);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}

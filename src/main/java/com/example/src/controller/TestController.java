e2b433c442ffc186b2237bb82648530600e81e29
hjun-park <phj0860@gmail.com>
2021-11-14 14:27:27 +0900

    DB 제거로 인해 재설정

--- src/main/java/com/example/src/controller/TestController.java
+++ src/main/java/com/example/src/controller/TestController.java
@@ -2,19 +2,14 @@ package com.example.src.controller;
 
 import com.example.src.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class TestController {
 
-	@Autowired
 	private final BoardService boardService;
-
-	public TestController(BoardService boardService) {
-		this.boardService = boardService;
-	}
 
 	@GetMapping("/test")
 	public String test() {
